d33070e44dd95dcfba58863b85ac0a2faf425087
fthibaud <fthibaud33@gmail.com>
2024-02-11 16:37:38 +0100

    Update dependencies and config file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,17 @@
 import dotenv from 'dotenv';
+import { cleanEnv, str } from 'envalid';
 
 dotenv.config();
 
-export const PREFIX: Array<string> = process.env.PREFIX?.split(",") ?? ["!"];
-export const BOT_TOKEN: string = process.env.BOT_TOKEN ?? "";
-export const OWNER_IDS: Array<string> = process.env.OWNER_IDS?.split(",") ?? [];
+const env = cleanEnv(process.env, {
+  PREFIX: str({ default: "!" }),
+  BOT_TOKEN: str(),
+  OWNER_IDS: str({ default: "" }),
+});
+
+// Convertir les chaînes PREFIX et OWNER_IDS en tableaux
+const PREFIX: Array<string> = env.PREFIX.split(",");
+const BOT_TOKEN: string = env.BOT_TOKEN;
+const OWNER_IDS: Array<string> = env.OWNER_IDS ? env.OWNER_IDS.split(",") : [];
+
+export { PREFIX, BOT_TOKEN, OWNER_IDS };
